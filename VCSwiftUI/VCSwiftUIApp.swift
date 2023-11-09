ecdd5096c784ab459e3e19d8978399db88eba3d8
Yenting Chen <yenting.chen@ziffmedia.com>
2023-11-09 07:51:36 +0100

    Initial Commit

---
+++ VCSwiftUI/VCSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  VCSwiftUIApp.swift
+//  VCSwiftUI
+//
+//  Created by Yenting Chen on 09/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct VCSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
